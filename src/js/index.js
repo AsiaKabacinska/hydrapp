4a653a3eb1cadbdef9de8104f9f089fbc8e69301
Asia Kabacińska <joannakabatek@gmail.com>
2021-07-09 09:22:40 +0200

    Hydrapp v.1.0.0

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,42 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log("HELLO 🚀");
+
+const glassCounter = document.querySelector(".glass__counter--js");
+const buttonAdd = document.querySelector(".button-add--js");
+const buttonRemove = document.querySelector(".button-remove--js");
+//uniwersalny klucz pod krótym trzymamy aktualne wartości
+const key = new Date().toLocaleString().slice(0, 10);
+
+let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+  currentGlassCounter = localStorageValue;
+} else {
+  localStorage.setItem(key, 0);
+}
+
+// wrzucam wartosc, która jest przypisan w currentGlassCounter (currentGlassCounter = localStorageValue;)
+glassCounter.innerHTML = currentGlassCounter;
+
+buttonAdd.addEventListener("click", () => {
+  currentGlassCounter++;
+  glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
+});
+
+buttonRemove.addEventListener("click", () => {
+  if (currentGlassCounter > 0) {
+    currentGlassCounter--;
+  }
+  glassCounter.innerHTML = currentGlassCounter;
+  localStorage.setItem(key, currentGlassCounter);
+});
